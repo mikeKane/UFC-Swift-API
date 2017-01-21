c256b0ea246dc45b274b79df47f953a57a7e79f9
Mike Kane <mjkane@comcast.net>
2017-01-21 11:36:49 -0500

    Add struct. Init class

--- UFCApi.swift
+++ UFCApi.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class UFCApi: NSObject {
-
-    let newsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/news"
-    let mediaURL = "http://ufc-data-api.ufc.com/api/v3/iphone/media"
-    let titleHoldersURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters/title_holders"
-    let fightersURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters"
-    let statsFilterKeyValueURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters/stats_filter_key_values"
-    let octagonGirlsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/octagon_girls"
-    let eventsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/events"
+struct APIStrings {
+    
+    static  let newsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/news"
+    static  let mediaURL = "http://ufc-data-api.ufc.com/api/v3/iphone/media"
+    static  let titleHoldersURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters/title_holders"
+    static  let fightersURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters"
+    static  let statsFilterKeyValueURL = "http://ufc-data-api.ufc.com/api/v3/iphone/fighters/stats_filter_key_values"
+    static  let octagonGirlsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/octagon_girls"
+    static  let eventsURL = "http://ufc-data-api.ufc.com/api/v3/iphone/events"
     
     //newsURL will produce an ID to be input for article
     func newsArticleURL(nID id: String) -> String {
@@ -30,6 +30,11 @@ class UFCApi: NSObject {
     //Born Country can have an empty string to bring back all countries.
     func fighterStats(wClass weightClass: String, bCountry bornCountry: String?) -> String {
         return "http://ufc-data-api.ufc.com/api/v3/iphone/fighters/stats/weight_class/:\(weightClass)/born_country/:\(bornCountry ?? "")"
+    }
+    
+    //Pass in a fighters name ex: conor-mcgregor. Must have dash. Must end in .json
+    func detailedFighterStats(fighter name: String) -> String {
+        return "http://nlds230.cdnak.neulion.com/fs/ufc/common/fighter/json/\(name).json"
     }
     
     //Pass in the fighter id returned from the fightersURL. Returns news and and media URLs in that order
@@ -47,4 +52,21 @@ class UFCApi: NSObject {
     func eventURL(eID id: String) -> (String,String,String,String,String) {
         return ("http://ufc-data-api.ufc.com/api/v3/iphone/events/\(id)","http://ufc-data-api.ufc.com/api/v3/iphone/events/:\(id)/fights", "http://ufc-data-api.ufc.com/api/v3/iphone/events/\(id)/media","http://ufc-data-api.ufc.com/api/v3/iphone/events/\(id)/highlights","http://ufc-data-api.ufc.com/api/v3/iphone/events/\(id)/tickets")
     }
+}
+
+class UFCApi: NSObject {
+    
+    //Holds the main UI data. Fighters, Events & TitleHolders.
+    var mainArray = [String]();
+    
+    override init() {
+        mainArray = [APIStrings.fightersURL,APIStrings.eventsURL,APIStrings.titleHoldersURL]
+    }
+    /* This will load the main table. When a user selects fighters it will display a large ist of fighters. I can parse them into sections if I wish with search etc. But lets get the data first. Once this finishes and saves, I will parse the .json file. I can then iterate over the names and pull the detailed stats. 
+    */
+    
+    func getJSOnObjects() {
+        
+    }
+    
 }
